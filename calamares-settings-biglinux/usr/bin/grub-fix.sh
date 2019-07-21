56616060e2f17f1dca47a20bc130436b996771f4
Bruno Goncalves <bigbruno@gmail.com>
2019-07-21 06:03:03 -0300

    ok

--- calamares-settings-biglinux/usr/bin/grub-fix.sh
+++ calamares-settings-biglinux/usr/bin/grub-fix.sh
@@ -7,8 +7,6 @@ echo 'GRUB_THEME=/boot/grub/themes/biglinux/theme.txt' >>  $*
 
 #Change default desktop in sddm to use in livecd
 echo "[Last]
-Session=$(cat /tmp/big_desktop_changed | sed 's|.*/||g')
-
-User=*" > $(echo "$*" | sed 's|etc/default/grub|var/lib/sddm/state.conf|g')
+Session=$(cat /tmp/big_desktop_changed | sed 's|.*/||g')" >> $(echo "$*" | sed 's|etc/default/grub|var/lib/sddm/state.conf|g')
 
 cp -f /tmp/big_desktop_theme $(echo "$*" | sed 's|etc/default/grub|etc/default-theme-biglinux|g')
